edf9af583b50e894be1b4974e766c1ba4df1e575
Sargon <sarka0159@gmail.com>
2022-12-01 15:03:14 +0100

    finishing up author class and made changes to book class

--- src/main/java/se/kth/databas/booksdb/model/Book.java
+++ src/main/java/se/kth/databas/booksdb/model/Book.java
@@ -1,6 +1,8 @@
 package se.kth.databas.booksdb.model;
 
+import java.sql.Array;
 import java.sql.Date;
+import java.util.ArrayList;
 
 /**
  * Representation of a book.
@@ -17,28 +19,50 @@ public class Book {
     // TODO: 
     // Add authors, as a separate class(!), and corresponding methods, to your implementation
     // as well, i.e. "private ArrayList<Author> authors;"
+    private ArrayList<Author> authors;
+
     
     public Book(int bookId, String isbn, String title, Date published) {
         this.bookId = bookId;
         this.isbn = isbn;
         this.title = title;
         this.published = published;
+        authors = new ArrayList<>();
     }
     
     public Book(String isbn, String title, Date published) {
         this(-1, isbn, title, published); 
     }
     
-    public int getBookId() { return bookId; }
-    public String getIsbn() { return isbn; }
-    public String getTitle() { return title; }
-    public Date getPublished() { return published; }
-    public String getStoryLine() { return storyLine; }
+    public int getBookId() {
+        return bookId;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Date getPublished() {
+        return published;
+    }
+    public String getStoryLine() {
+        return storyLine;
+    }
     
     public void setStoryLine(String storyLine) {
         this.storyLine = storyLine;
     }
-    
+
+    public ArrayList<Author> getAuthors() {
+        return (ArrayList<Author>) authors.clone();
+    }
+    public void addAuthor(Author author){
+        if (author!= null){
+            authors.add(author);
+        }
+    }
+
     @Override
     public String toString() {
         return title + ", " + isbn + ", " + published.toString();
---
+++ src/main/java/se/kth/databas/booksdb/model/testingClasses.java
@@ -0,0 +1,7 @@
+package se.kth.databas.booksdb.model;
+
+public class testingClasses {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}

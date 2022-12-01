d6a3db51eae4ec2ded79c12af4d857cb8a8fbbfe
Sargon <sarka0159@gmail.com>
2022-12-01 14:41:29 +0100

    finishing up author class

--- src/main/java/se/kth/databas/booksdb/model/Author.java
+++ src/main/java/se/kth/databas/booksdb/model/Author.java
@@ -6,4 +6,45 @@ public class Author {
     private String firstName;
     private String lastName;
 
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "authorId=" + authorId +
+                ", isbn='" + isbn + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
